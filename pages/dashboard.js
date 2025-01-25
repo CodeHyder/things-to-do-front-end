328efbacf486e3dd6f4ff3326329410baf177dc8
raphael <rapharodriguescm20@gmail.com>
2025-01-24 22:25:55 -0300

    refactor: ajustando layout do dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -28,11 +28,11 @@ const Dashboard = () => {
             >
                 Sair
             </Button>
-            <h1 className='text-3xl font-bold text-gray-800 mb-6 text-center'>Bem vindo {username}</h1>
 
+            <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">Things to Do</h2>
             <div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg p-6">
-                <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">Things to Do</h2>
-                <CreateTask addTask={addTask} onTaskLoading={taskLoading}/>
+                <h1 className='text-3xl font-bold text-gray-800 mb-6 text-center'>Bem vindo {username}</h1>
+                <CreateTask addTask={addTask} onTaskLoading={taskLoading} />
                 <TaskList
                     tasks={tasks}
                     setTasks={setTasks}
